6aed37c6cccde5c5896f3dbf2d83da61f78d1649
Glitch (tart-cemetery) <none>
2018-04-25 19:15:45 +0000

    🐿⛈ Checkpoint ./foo.php:86128/79 ./index.php:86128/116 ./.htaccess:86128/8

--- index.php
+++ index.php
@@ -3,13 +3,6 @@
     <title>LAMP Proof of Concept</title>
   </head>
   <body>
-
-<?php
- for ($x = 0; $x <= 10; $x++) {
-    echo "The number is: $x <br>";
- } 
-?>
-
-    <?php phpinfo(); ?>
+    This is a test.
   </body>
 </html>
