a276a1394a7db6f502334a55bdfca408dad16cff
Glitch (tart-cemetery) <none>
2019-06-03 17:45:23 +0000

    🍂⛪️ Checkpoint ./views/header.html:86128/154 ./views/index.php:86128/88

--- views/cache_index.php
+++ views/cache_index.php
@@ -2,9 +2,9 @@
 <html>
   <head>
     <title>To do list model 2</title>
-    <link href="/static/style.css" rel="stylesheet" crossorigin="anonymous">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="/static/style.css" rel="stylesheet" crossorigin="anonymous">
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script>
     <script
   src="https://code.jquery.com/jquery-3.4.1.min.js"
@@ -44,22 +44,22 @@
   <div class="col-lg-8 offset-2">
     <h2>Current To Do:</h2>
       
-    <table class="table table-striped table-bordered">
+    <table class="table table-striped" style="border:1">
       <thead class="thead-dark">
         <tr>
           <th>Description</th>
-          <th class="min"></th>
+          <th></th>
         </tr>
       </thead>
       <tbody>
 <?php  foreach ($todos as $todo) : ?>
         <tr>
           <td class="align-middle"><?php echo "{$todo['description']}" ?></td>
-          <td class="min">
+          <td>
             <div class="btn-toolbar align-middle float-right">
-              <button class="btn btn-secondary d-flex justify-content-center align-content-between mr-1 addclickhandler" action="done.php" stu_num="<?php echo "{$todo['id']}"?>"><span class="material-icons">done</span></button>
-              <button class="btn btn-secondary d-flex justify-content-center align-content-between mr-1 addclickhandler" action="edit.php" stu_num="<?php echo "{$todo['id']}"?>"><span class="material-icons">mode_edit</span></button>
-              <button class="btn btn-secondary d-flex justify-content-center align-content-between addclickhandler" action="delete.php" stu_num="<?php echo "{$todo['id']}"?>"><span class="material-icons">delete</span></button>
+              <button class="btn btn-secondary d-flex justify-content-center align-content-between bg-success mr-1 addclickhandler" action="done.php" stu_num="<?php echo "{$todo['id']}"?>"><span class="material-icons">done</span></button>
+              <button class="btn btn-secondary d-flex justify-content-center align-content-between bg-primary mr-1 addclickhandler" action="edit.php" stu_num="<?php echo "{$todo['id']}"?>"><span class="material-icons">mode_edit</span></button>
+              <button class="btn btn-secondary d-flex justify-content-center align-content-between bg-danger addclickhandler" action="delete.php" stu_num="<?php echo "{$todo['id']}"?>"><span class="material-icons">delete</span></button>
             </div>
           </td>
         </tr>
--- views/index.php
+++ views/index.php
@@ -21,22 +21,22 @@
   <div class="col-lg-8 offset-2">
     <h2>Current To Do:</h2>
       
-    <table class="table table-striped table-bordered">
+    <table class="table table-striped" style="border:1">
       <thead class="thead-dark">
         <tr>
           <th>Description</th>
-          <th class="min"></th>
+          <th></th>
         </tr>
       </thead>
       <tbody>
 [[ foreach ($todos as $todo) : ]]
         <tr>
           <td class="align-middle"><?php echo "{$todo['description']}" ?></td>
-          <td class="min">
+          <td>
             <div class="btn-toolbar align-middle float-right">
-              <button class="btn btn-secondary d-flex justify-content-center align-content-between mr-1 addclickhandler" action="done.php" stu_num="<?php echo "{$todo['id']}"?>"><span class="material-icons">done</span></button>
-              <button class="btn btn-secondary d-flex justify-content-center align-content-between mr-1 addclickhandler" action="edit.php" stu_num="<?php echo "{$todo['id']}"?>"><span class="material-icons">mode_edit</span></button>
-              <button class="btn btn-secondary d-flex justify-content-center align-content-between addclickhandler" action="delete.php" stu_num="<?php echo "{$todo['id']}"?>"><span class="material-icons">delete</span></button>
+              <button class="btn btn-secondary d-flex justify-content-center align-content-between bg-success mr-1 addclickhandler" action="done.php" stu_num="<?php echo "{$todo['id']}"?>"><span class="material-icons">done</span></button>
+              <button class="btn btn-secondary d-flex justify-content-center align-content-between bg-primary mr-1 addclickhandler" action="edit.php" stu_num="<?php echo "{$todo['id']}"?>"><span class="material-icons">mode_edit</span></button>
+              <button class="btn btn-secondary d-flex justify-content-center align-content-between bg-danger addclickhandler" action="delete.php" stu_num="<?php echo "{$todo['id']}"?>"><span class="material-icons">delete</span></button>
             </div>
           </td>
         </tr>
