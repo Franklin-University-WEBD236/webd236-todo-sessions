81dad01359b8a7afad72926a6d719a64d25a5ddb
Glitch (tart-cemetery) <none>
2019-06-03 17:34:57 +0000

    💖🍈 Checkpoint ./views/index.php:86128/1434 ./static/style.css:86128/138

--- views/index.php
+++ views/index.php
@@ -21,20 +21,20 @@
   <div class="col-lg-8 offset-2">
     <h2>Current To Do:</h2>
       
-    <table class="table table-striped">
+    <table class="table table-striped table-bordered">
       <thead class="thead-dark">
         <tr>
           <th>Description</th>
-          <th></th>
+          <th class="min"></th>
         </tr>
       </thead>
       <tbody>
 [[ foreach ($todos as $todo) : ]]
         <tr>
           <td class="align-middle"><?php echo "{$todo['description']}" ?></td>
-          <td>
+          <td class="min">
             <div class="btn-toolbar align-middle float-right">
-              <button class="btn btn-secondary d-flex justify-content-center align-content-between mr-1 addclickhandler" action="view.php" stu_num="<?php echo "{$todo['id']}"?>"><span class="material-icons">description</span></button>
+              <button class="btn btn-secondary d-flex justify-content-center align-content-between mr-1 addclickhandler" action="done.php" stu_num="<?php echo "{$todo['id']}"?>"><span class="material-icons">done</span></button>
               <button class="btn btn-secondary d-flex justify-content-center align-content-between mr-1 addclickhandler" action="edit.php" stu_num="<?php echo "{$todo['id']}"?>"><span class="material-icons">mode_edit</span></button>
               <button class="btn btn-secondary d-flex justify-content-center align-content-between addclickhandler" action="delete.php" stu_num="<?php echo "{$todo['id']}"?>"><span class="material-icons">delete</span></button>
             </div>
@@ -62,7 +62,7 @@
           <td class="align-middle"><?php echo "{$todo['description']}" ?></td>
           <td>
             <div class="btn-toolbar align-middle float-right">
-              <button class="btn btn-secondary d-flex justify-content-center align-content-between mr-1 addclickhandler" action="view.php" stu_num="<?php echo "{$todo['id']}"?>"><span class="material-icons">visibility</span></button>
+              <button class="btn btn-secondary d-flex justify-content-center align-content-between mr-1 addclickhandler" action="done.php" stu_num="<?php echo "{$todo['id']}"?>"><span class="material-icons">done</span></button>
               <button class="btn btn-secondary d-flex justify-content-center align-content-between mr-1 addclickhandler" action="edit.php" stu_num="<?php echo "{$todo['id']}"?>"><span class="material-icons">mode_edit</span></button>
               <button class="btn btn-secondary d-flex justify-content-center align-content-between addclickhandler" action="delete.php" stu_num="<?php echo "{$todo['id']}"?>"><span class="material-icons">delete</span></button>
             </div>
