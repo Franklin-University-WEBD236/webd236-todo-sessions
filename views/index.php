726a88d21e1b957889fc2a5515cac67636afc237
Glitch (tart-cemetery) <none>
2019-06-03 15:14:18 +0000

    🐕🛢 Checkpoint ./urlrouter.php:86128/2 ./php.ini:86128/142344 ./lamp-start.sh:86128/303 ./.htaccess:86128/3 ./views/header.html:86128/1576 ./views/footer.html:86128/525 ./views/index.php:86128/565

--- views/index.php
+++ views/index.php
@@ -1,27 +1,32 @@
 %% views/header.html %%
 
-<h1>{{$title}}</h1>
-<form action="@@todo/add@@" method="post">
-    <label for="description">Description:</label>
-    <input type="text" id="description" name="description" />
-    <input type="submit" value="Add" />
-</form>
-<h2>Current To Do:</h2>
-<ol>
-    [[ foreach ($todos as $todo) : ]]
-    <li>
-        <a href="@@todo/view/{{$todo['id']}}@@">[View]</a> <a href="@@todo/edit/{{$todo['id']}}@@">[Edit]</a> <a href="@@todo/delete/{{$todo['id']}}@@">[Del]</a> {{$todo['description']}}
-    </li>
-    [[ endforeach; ]]
-</ol>
+<div class="row">
+  <div class="col-lg-8 offset-2">
 
-<h2>Past To Do:</h2>
-<ol>
-    [[ foreach ($dones as $todo) : ]]
-    <li>
-        <a href="@@todo/view/{{$todo['id']}}@@">[View]</a> <a href="@@todo/edit/{{$todo['id']}}@@">[Edit]</a> <a href="@@todo/delete/{{$todo['id']}}@@">[Del]</a> {{$todo['description']}}
-    </li>
-    [[ endforeach; ]]
-</ol>
+    <h1>{{$title}}</h1>
+    <form action="@@todo/add@@" method="post">
+        <label for="description">Description:</label>
+        <input type="text" id="description" name="description" />
+        <input type="submit" value="Add" />
+    </form>
+    <h2>Current To Do:</h2>
+    <ol>
+        [[ foreach ($todos as $todo) : ]]
+        <li>
+            <a href="@@todo/view/{{$todo['id']}}@@">[View]</a> <a href="@@todo/edit/{{$todo['id']}}@@">[Edit]</a> <a href="@@todo/delete/{{$todo['id']}}@@">[Del]</a> {{$todo['description']}}
+        </li>
+        [[ endforeach; ]]
+    </ol>
 
+    <h2>Past To Do:</h2>
+    <ol>
+        [[ foreach ($dones as $todo) : ]]
+        <li>
+            <a href="@@todo/view/{{$todo['id']}}@@">[View]</a> <a href="@@todo/edit/{{$todo['id']}}@@">[Edit]</a> <a href="@@todo/delete/{{$todo['id']}}@@">[Del]</a> {{$todo['description']}}
+        </li>
+        [[ endforeach; ]]
+    </ol>
+  </div>
+</div>
+          
 %% views/footer.html %% 
