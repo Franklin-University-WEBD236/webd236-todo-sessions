ce9cac0a4951e4da13e86be03d205472a4991080
Glitch (tart-cemetery) <none>
2019-06-04 20:57:27 +0000

    👣🍳 Checkpoint ./router.php:86128/350

--- router.php
+++ router.php
@@ -1,36 +1,36 @@
 <?php
 function routeUrl() {
-    $method = $_SERVER['REQUEST_METHOD'];
-    $requestURI = explode('/', rtrim($_SERVER['REQUEST_URI'], '?'));
-    $scriptName = explode('/', $_SERVER['SCRIPT_NAME']);
+  $method = $_SERVER['REQUEST_METHOD'];
+  $requestURI = explode('/', $_SERVER['REQUEST_URI']);
+  $scriptName = explode('/', $_SERVER['SCRIPT_NAME']);
 
-    for ($i = 0; $i < sizeof($scriptName); $i++) {
-        if ($requestURI[$i] == $scriptName[$i]) {
-            unset($requestURI[$i]);
-        }
+  for ($i = 0; $i < sizeof($scriptName); $i++) {
+    if ($requestURI[$i] == $scriptName[$i]) {
+      unset($requestURI[$i]);
     }
-    # continued...
-    
-    $entity = array_values($requestURI);
-    $controller = 'controllers/' . $entity[0] . '.php';
-    $func = strtolower($method) . '_' . (isset($entity[1]) ? $entity[1] : 'index');
-    $params = array_slice($entity, 2);
+  }
+  # continued...
 
-    error_log("Looking for controller ${controller}", 0);
+  $entity = array_values($requestURI);
+  $controller = 'controllers/' . $entity[0] . '.php';
+  $func = strtolower($method) . '_' . (isset($entity[1]) ? $entity[1] : 'index');
+  $params = array_slice($entity, 2);
 
-    if (!file_exists($controller)) {
-      die("Controller '$controller' doesn't exist.");
-    }
-    require $controller;
-    if (!function_exists($func)) {
-      die("Function '$func' doesn't exist.");
-    }
+  error_log("Looking for controller ${controller}", 0);
+
+  if (!file_exists($controller)) {
+    die("Controller '$controller' doesn't exist.");
+  }
+  require $controller;
+  if (!function_exists($func)) {
+    die("Function '$func' doesn't exist.");
+  }
 echo "<pre>";
 print_r($_SERVER['REQUEST_METHOD']);
 echo "<br>";
-print_r($_SERVER['REQUEST_URI']);
+print_r(explode('/', $_SERVER['REQUEST_URI']));
 echo "<br>";
-print_r($_SERVER['SCRIPT_NAME']);
+print_r(explode('/', $_SERVER['SCRIPT_NAME']));
 echo "<br>";
 echo "</pre>";
 
