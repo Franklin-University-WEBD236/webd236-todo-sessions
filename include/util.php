bb4c1b404293e0a162712225c79221c4c67333a1
Glitch (tart-cemetery) <none>
2019-06-05 21:51:30 +0000

    🚍🌼 Checkpoint ./include/util.php:86128/1037 ./router.php:86128/2

--- include/util.php
+++ include/util.php
@@ -8,91 +8,89 @@ function safeParam($arr, $index, $default) {
 }
 
 function debug($something) {
-    echo "<div class='debug'>\n";
-    print_r($something);
-    echo "\n</div>\n";
+  echo "<div class='debug'>\n";
+  print_r($something);
+  echo "\n</div>\n";
 }
 
 function redirect($url) {
-    header("Location: $url");
-    exit();
+  header("Location: $url");
+  exit();
 }
 
 function redirectRelative($url) {
-    redirect(url($url));
+  redirect(url($url));
 }
 
 function url($url) {
-    $requestURI = explode('/', $_SERVER['REQUEST_URI']);
-    $scriptName = explode('/', $_SERVER['SCRIPT_NAME']);
+  $requestURI = explode('/', $_SERVER['REQUEST_URI']);
+  $scriptName = explode('/', $_SERVER['SCRIPT_NAME']);
 
-    $dir = array();
-    for ($i = 0; $i < sizeof($scriptName); $i++) {
-        if ($requestURI[$i] == $scriptName[$i]) {
-            $dir[] = $requestURI[$i];
-        } else {
-            break;
-        }
+  $dir = array();
+  for ($i = 0; $i < sizeof($scriptName); $i++) {
+    if ($requestURI[$i] == $scriptName[$i]) {
+      $dir[] = $requestURI[$i];
+    } else {
+      break;
     }
-    return implode('/', $dir) . '/' . $url;
+  }
+  return implode('/', $dir) . '/' . $url;
 }
 
 function __importTemplate($matches) {
-    $fileName = trim($matches[1]);
-    if (!file_exists($fileName)) {
-        die("Template $fileName doesn't exist.");
-    }
-    $contents = file_get_contents($fileName);
-    $contents = preg_replace_callback('/%%\s*(.*)\s*%%/', '__importTemplate', $contents);
-    return $contents;
+  $fileName = trim($matches[1]);
+  if (!file_exists($fileName)) {
+    die("Template $fileName doesn't exist.");
+  }
+  $contents = file_get_contents($fileName);
+  $contents = preg_replace_callback('/%%\s*(.*)\s*%%/', '__importTemplate', $contents);
+  return $contents;
 }
 
 function __resolveRelativeUrls($matches) {
-    return url($matches[1]);
+  return url($matches[1]);
 }
 
 function __cacheName($view) {
-    $cachePath = explode('/', $view);
-    $idx = sizeof($cachePath) - 1;
-    $cachePath[$idx] = 'cache_' . $cachePath[$idx];
-    return implode('/', $cachePath);
+  $cachePath = explode('/', $view);
+  $idx = sizeof($cachePath) - 1;
+  $cachePath[$idx] = 'cache_' . $cachePath[$idx];
+  return implode('/', $cachePath);
 }
 
 function renderTemplate($view, $params) {
-    $useCache = false;
-  
-    if (!file_exists($view)) {
-        die("File $view doesn't exist.");
-    }
-    # do we have a cached version?
-    clearstatcache();
-    $cacheName = __cacheName($view);
-    if ($useCache && file_exists($cacheName) && (filemtime($cacheName) >= filemtime($view))) {
-        $contents = file_get_contents($cacheName);
-    } else {
-        # we need to build the file (doesn't exist or template is newer)
-        $contents = __importTemplate(array('unused', $view));
+  $useCache = false;
 
-        $contents = preg_replace_callback('/@@\s*(.*)\s*@@/U', '__resolveRelativeUrls', $contents);
+  if (!file_exists($view)) {
+    die("File $view doesn't exist.");
+  }
+  # do we have a cached version?
+  clearstatcache();
+  $cacheName = __cacheName($view);
+  if ($useCache && file_exists($cacheName) && (filemtime($cacheName) >= filemtime($view))) {
+    $contents = file_get_contents($cacheName);
+  } else {
+    # we need to build the file (doesn't exist or template is newer)
+    $contents = __importTemplate(array('unused', $view));
 
-        // @formatter:off
-        $patterns = array(
-            array('src' => '/{{/', 'dst' => '<?php echo('),
-            array('src' => '/}}/', 'dst' => '); ?>'),
-            array('src' => '/\[\[/', 'dst' => '<?php '),
-            array('src' => '/\]\]/', 'dst' => '?>')
-        );
-        // @formatter:on
-        foreach ($patterns as $pattern) {
-            $contents = preg_replace($pattern['src'], $pattern['dst'], $contents);
-        }
-        file_put_contents($cacheName, $contents);
+    $contents = preg_replace_callback('/@@\s*(.*)\s*@@/U', '__resolveRelativeUrls', $contents);
+
+    $patterns = array(
+      array('src' => '/{{/', 'dst' => '<?php echo('),
+      array('src' => '/}}/', 'dst' => '); ?>'),
+      array('src' => '/\[\[/', 'dst' => '<?php '),
+      array('src' => '/\]\]/', 'dst' => '?>')
+    );
+    foreach ($patterns as $pattern) {
+      $contents = preg_replace($pattern['src'], $pattern['dst'], $contents);
     }
-    extract($params);
-    ob_start();
-    eval("?>" . $contents);
-    $result = ob_get_contents();
-    ob_end_clean();
-    echo $result;
+    file_put_contents($cacheName, $contents);
+  }
+  extract($params);
+  ob_start();
+  eval("?>" . $contents);
+  $result = ob_get_contents();
+  ob_end_clean();
+  echo $result;
 }
 ?>
