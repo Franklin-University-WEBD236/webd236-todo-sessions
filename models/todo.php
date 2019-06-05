5ea29c802acf6e516e827359d60b341f41bb1679
Glitch (tart-cemetery) <none>
2019-06-05 20:59:36 +0000

    🐓🌮 Checkpoint ./models/todo.php:86128/188

--- models/todo.php
+++ models/todo.php
@@ -2,33 +2,33 @@
 include_once 'models/db.php';
 
 function findToDoById($id) {
-    global $db;
-    $st = $db -> prepare('SELECT * FROM todo WHERE id = ?');
-    $st -> bindParam(1, $id);
-    $st -> execute();
-    return $st -> fetch(PDO::FETCH_ASSOC);
+  global $db;
+  $st = $db -> prepare('SELECT * FROM todo WHERE id = ?');
+  $st -> bindParam(1, $id);
+  $st -> execute();
+  return $st -> fetch(PDO::FETCH_ASSOC);
 }
 
 function findAllCurrentToDos() {
-    global $db;
-    $st = $db -> prepare('SELECT * FROM todo WHERE done = 0 ORDER BY id');
-    $st -> execute();
-    return $st -> fetchAll(PDO::FETCH_ASSOC);
+  global $db;
+  $st = $db -> prepare('SELECT * FROM todo WHERE done = 0 ORDER BY id');
+  $st -> execute();
+  return $st -> fetchAll(PDO::FETCH_ASSOC);
 }
 
 function findAllDoneToDos() {
-    global $db;
-    $st = $db -> prepare('SELECT * FROM todo WHERE done != 0 ORDER BY id');
-    $st -> execute();
-    return $st -> fetchAll(PDO::FETCH_ASSOC);
+  global $db;
+  $st = $db -> prepare('SELECT * FROM todo WHERE done != 0 ORDER BY id');
+  $st -> execute();
+  return $st -> fetchAll(PDO::FETCH_ASSOC);
 }
 
 function addToDo($description) {
-    global $db;
-    $statement = $db -> prepare("INSERT INTO todo (description, done) values (?, 0)");
-    $statement -> bindParam(1, $description);
-    $statement -> execute();
-    return $db->lastInsertId();
+  global $db;
+  $statement = $db -> prepare("INSERT INTO todo (description, done) values (?, 0)");
+  $statement -> bindParam(1, $description);
+  $statement -> execute();
+  return $db->lastInsertId();
 }
 
 function toggleDoneToDo($id) {
@@ -37,18 +37,18 @@ function toggleDoneToDo($id) {
 }
 
 function updateToDo($id, $description, $done) {
-    global $db;
-    $statement = $db -> prepare("UPDATE todo SET description = ?, done = ? WHERE id = ?");
-    $statement -> bindParam(1, $description);
-    $statement -> bindParam(2, $done);
-    $statement -> bindParam(3, $id);
-    $statement -> execute();
+  global $db;
+  $statement = $db -> prepare("UPDATE todo SET description = ?, done = ? WHERE id = ?");
+  $statement -> bindParam(1, $description);
+  $statement -> bindParam(2, $done);
+  $statement -> bindParam(3, $id);
+  $statement -> execute();
 }
 
 function deleteToDo($id) {
-    global $db;
-    $statement = $db -> prepare("DELETE FROM todo WHERE id = ?");
-    $statement -> bindParam(1, $id);
-    $statement -> execute();
+  global $db;
+  $statement = $db -> prepare("DELETE FROM todo WHERE id = ?");
+  $statement -> bindParam(1, $id);
+  $statement -> execute();
 }
 ?>
