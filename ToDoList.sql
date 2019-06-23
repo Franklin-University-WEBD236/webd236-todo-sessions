5da91bc979af7cdc06e655b763d7adbd20efe593
Glitch (tart-cemetery) <none>
2019-06-23 19:45:06 +0000

    🐌🌵 Checkpoint ./.htaccess:86128/242 ./ToDoList.sql:86128/349

--- ToDoList.sql
+++ ToDoList.sql
@@ -1,20 +1,25 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
 DROP TABLE IF EXISTS `todo`;
+DROP TABLE IF EXISTS `user`;
 
-CREATE TABLE `todo`
--- This table created by SQLite2009 Pro Enterprise Manager 
--- Osen Kusnadi - http://www.osenxpsuite.net 
--- Create date:2012-01-17 11:18:09
-(
-       description VARCHAR(50) NOT NULL,
-       done INTEGER NOT NULL,
-       id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT
+CREATE TABLE `todo` (
+  description VARCHAR(50) NOT NULL,
+  done INTEGER NOT NULL,
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT
 );
 INSERT INTO "todo" VALUES('Prepare a model 1 architecture example',1,2);
 INSERT INTO "todo" VALUES('Teach class on Wednesday, 7:30 PM EST.',1,5);
 INSERT INTO "todo" VALUES('Prepare a model 2 architecture example',1,6);
 INSERT INTO "todo" VALUES('Get this sample app working on Glitch',0,8);
+
+CREATE TABLE `user` (
+  id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  email TEXT NOT NULL,
+  password TEXT NOT NULL,
+  
+);
+
 DELETE FROM sqlite_sequence;
 INSERT INTO "sqlite_sequence" VALUES('todo',10);
 COMMIT;
