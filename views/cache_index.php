b9aadecaddd547074448c49d6fcab9ac3c86c887
Glitch (tart-cemetery) <none>
2019-06-25 15:56:49 +0000

    💈💄 Checkpoint ./views/header.html:86128/292 ./static/style.css:86128/107

--- views/cache_index.php
+++ views/cache_index.php
@@ -72,12 +72,15 @@
 <?php  endif;?>
       
 <?php  if (isset($_SESSION['flash'])): ?>
-<div class="alert alert-success alert-dismissible fade show" role="alert">
+<div class="alert alert-success alert-dismissible fade show flash-message" role="alert" id="flash">
   <?php echo($_SESSION['flash']); ?>
   <button type="button" class="close" data-dismiss="alert" aria-label="Close">
     <span aria-hidden="true">&times;</span>
   </button>
 </div>
+<script type="text/javascript">
+  $("#flash").fadeOut(3000);
+</script>
 <?php  
    unset($_SESSION['flash']);
    endif;
